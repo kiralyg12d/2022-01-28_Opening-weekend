ac29a5ac8dab3dce1eba491febc2a72fb40cb5d1
kiralyg12d <95190713+kiralyg12d@users.noreply.github.com>
2022-01-28 09:10:30 +0100

    Add project files.

---
+++ 2022-01-28_Opening-weekend/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
